5dbf10e6efff475b856c143f8543fef32581573e
KGVWijesooriya <vimukthi200020@gmail.com>
2022-10-02 11:13:43 +0530

    find largest

--- php/KGVJWijesooriya.php
+++ php/KGVJWijesooriya.php
@@ -5,4 +5,11 @@ function removeDuplicateEl($array){
 }
 
 print_r(removeDuplicateEl([1,4,5,4,2,5,7,3]));
+
+function findLargestNum($array){
+    return print_r(max($array));
+}
+
+findLargestNum([54,23,65,76,12,4]);
+
 ?>
